006c8777f1099fde3e7e57136b9f57e2ee21d663
juniorArias02 <junior.arias02yt@gmail.com>
2025-07-24 14:21:45 -0500

    correcion de imagen

--- src/api/conexion.js
+++ src/api/conexion.js
@@ -1,4 +1,4 @@
 // export const URL_ENDPOINT = 'http://localhost:8888';
 // export const PATH_IMAGEN = 'http://localhost:8888';
 export const URL_ENDPOINT = 'https://psicologicamentehablando.space/AgendamientoApi';
-export const PATH_IMAGEN = 'https://psicologicamentehablando.space';
+export const PATH_IMAGEN = 'https://psicologicamentehablando.space/AgendamientoApi';
