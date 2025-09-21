5ec3f71e23b94e9143fe0d4cb3784c20a0f84e79
juniorArias02 <junior.arias02yt@gmail.com>
2025-09-21 21:25:43 +0200

    correcion

--- src/components/pages/TusCitas.jsx
+++ src/components/pages/TusCitas.jsx
@@ -191,7 +191,7 @@ export default function TusCitas() {
             .filter((cita) => !filtroEstado || cita.estado_cita === filtroEstado)
             .map((cita, index) => {
               const nombre = usuario.rol === "paciente" ? cita.psicologa : cita.paciente;
-              const telefono = usuario.rol === "paciente" ? cita.telefono_psicologa : cita.telefono_paciente;
+              const telefono = usuario.rol === "paciente" ? cita.psicologa_telefono : cita.paciente_telefono;
               const { fecha, hora } = formatearFechaHoraSeparado(cita.fecha_hora_utc);
 
               return (
